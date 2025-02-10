7d19d0415509821e1c3748789a7a148f95ce6eca
coderxi1 <coderxi@qq.com>
2025-02-10 10:45:19 +0800

    feat: add markmap in index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMarkmap from 'remark-markmap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,4 +25,9 @@ export default defineConfig({
 			favicon: '/favicon.png',
 		}),
 	],
+	markdown: {
+		remarkPlugins: [
+			[remarkMarkmap,{darkThemeCssSelector: '[data-theme=dark]'}]
+		]
+	}
 });
